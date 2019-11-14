1a47b4b119053b4b2bd0e07d9dfc6bb3f5ec146a
kyle1elyk <steadkyle@gmail.com>
2019-11-14 09:30:57 -0500

    Updated with provided files from course website. Slightly modified to resolve formatting and resource leaks.

--- CSE2010TermProject_Boggle/src/BogglePlayer.java
+++ CSE2010TermProject_Boggle/src/BogglePlayer.java
@@ -1,13 +1,41 @@
-/**
- * 
- * @author Kyle Stead, Justyn Diaz
- *
- */
+/*
+
+  Authors (group members):
+  Email addresses of group members:
+  Group name:
+
+  Course:
+  Section:
+
+  Description of the overall algorithm and key data structures:
+
+
+*/
+
 public class BogglePlayer {
 
-	public static void main(final String[] args) {
-		
+	// initialize BogglePlayer with a file of English words
+	public BogglePlayer(String wordFile) {
 
+	}
+
+	// based on the board, find valid words
+	//
+	// board: 4x4 board, each element is a letter, 'Q' represents "QU",
+	// first dimension is row, second dimension is column
+	// ie, board[row][col]
+	//
+	// Return at most 20 valid words in UPPERCASE and
+	// their paths of locations on the board in myWords;
+	// Use null if fewer than 20 words.
+	//
+	// See Word.java for details of the Word class and
+	// Location.java for details of the Location class
+
+	public Word[] getWords(char[][] board) {
+		Word[] myWords = new Word[20];
+
+		return myWords;
 	}
 
 }
