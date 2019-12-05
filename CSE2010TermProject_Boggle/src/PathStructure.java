943d86d442330bbd472d85c8b6b06c18430c2529
Justyn <Justyn@DESKTOP-60KAMST>
2019-12-04 23:32:09 -0500

    Added documentation for PathStructure.java

--- CSE2010TermProject_Boggle/src/PathStructure.java
+++ CSE2010TermProject_Boggle/src/PathStructure.java
@@ -1,31 +1,45 @@
 import java.util.Iterator;
 
-
 /**
- * Linked List for keeping track and updating the path of a word on the boggle board.
+ * Array based structure for keeping track and updating the path of a word on the boggle board.
  * 
  * @author Kyle Stead, Justyn Diaz 
  */
 public class PathStructure implements Iterable<PathStructure.Node>{
-public final byte[] path;
+	public final byte[] path;
 	
 	public class Node {
 		final byte xy;
+		
 		private Node(byte xy) {
 			this.xy = xy;
 		}
+		
+		/*
+		 *  Returns the X and Y value.
+		 *  X location: 0000XX00
+		 *  Y location: 000000YY
+		 */
 		public int[] getXY() {
 			return new int[] {(xy & 0b1100) >>> 2, xy & 0b11};
 		}
-		
 	}
 	
 	public PathStructure() {
 		this(0);
 	}
+	
 	public PathStructure(int length) {
 		path = new byte[length];
 	}
+	
+	/**
+	 * Clones the path structure, and adds the passed in x and y integers.
+	 * 
+	 * @param x, y Coordinates of a given tile.
+	 * @return Cloned The updated path.
+	 * @throws IllegalArgumentException If the coordinates are out of bounds.
+	 */
 	public PathStructure cloneAdd(int x, int y) {
 		if (x > 3 || x < 0 || y > 3 || y < 0) throw new IllegalArgumentException();
 		
@@ -35,15 +49,16 @@ public final byte[] path;
 		}
 		
 		cloned.path[this.path.length] = (byte) ((byte) (x << 2) + (byte)(y));
-		//System.out.println(cloned.path[this.path.length]);
 		return cloned;
 	}
 	
-	
+	// Returns the path.
 	public byte[] getPath() {
 		return path;
 	}
+	
 	@Override
+	// Iterator for traveling through each position in the path.
 	public Iterator<Node> iterator() {
 		return new Iterator<Node>() {
 			int pos = 0;
@@ -51,16 +66,13 @@ public final byte[] path;
 			
 			@Override
 			public boolean hasNext() {
-				
 				return pos < path.length;
 			}
 
 			@Override
 			public Node next() {
-				
 				return new Node(path[pos++]);
 			}
-			
 		};
 	}
 }
